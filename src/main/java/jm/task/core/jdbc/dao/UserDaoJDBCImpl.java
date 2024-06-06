1d29f23c4d02b66d33d19efe91b7adbb4280c871
Fardoc-git <163406460+Fardoc-git@users.noreply.github.com>
2024-06-06 15:16:47 +0300

    1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +13,107 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
+        String expression = """
+                CREATE TABLE dbtest.users
+                (
+                    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(45) NOT NULL,
+                    lastName VARCHAR(45) NOT NULL,
+                    age INT(3) NULL
+                )
+                    ENGINE = InnoDB
+                    DEFAULT CHARACTER SET = utf8;
+                """;
 
+        try (Connection connection = Util.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(expression);
+
+            System.out.println("table created");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void dropUsersTable() {
+        String expression = "DROP TABLE dbtest.users;";
 
+        try (Connection connection = Util.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(expression);
+
+            System.out.println("table deleted");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String expression = "INSERT INTO dbtest.users (name, lastName, age) VALUES (?, ?, ?);";
 
+        try (Connection connection = Util.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(expression);
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+
+            System.out.println("table updated");
+        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
+        String expression = "DELETE FROM dbtest.users WHERE id = ?;";
+
+        try (Connection connection = Util.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(expression);
+            statement.setLong(1, id);
+            statement.executeUpdate();
+
+            System.out.println("table updated");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String expression = "SELECT * FROM dbtest.users;";
+        ResultSet rs = null;
+        List<User> userList = new ArrayList<>();
+
+        try (Connection connection = Util.getConnection()) {
+            Statement statement = connection.createStatement();
+            rs = statement.executeQuery(expression);
+            while (rs.next()) {
+                User user = new User(rs.getString("name")
+                        ,rs.getString("lastName")
+                        ,rs.getByte("age"));
+                user.setId(rs.getLong("id"));
+                userList.add(user);
+            }
+
+            System.out.println("got all users");
+        } catch (SQLException | NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return userList;
     }
 
     public void cleanUsersTable() {
+        String expression = "DELETE FROM dbtest.users;";
 
+        try (Connection connection = Util.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(expression);
+
+            System.out.println("table deleted");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    public static Connection getConnection() throws SQLException {
+        String hostName = "jdbc:mysql://localhost:3306";
+        String userName = "root";
+//        Scanner scanner = new Scanner(System.in);
+//        String password = scanner.nextLine();
+        String password = "root";
+        return getConnection(hostName, userName, password);
+    }
+
+    public static Connection getConnection(String hostName, String userName, String password) throws SQLException {
+        return DriverManager.getConnection(hostName, userName, password);
+    }
 }
