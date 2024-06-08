1c5b5777d1f8e9b00d4c1cf1093173fdf2a9ac5f
Fardoc-git <163406460+Fardoc-git@users.noreply.github.com>
2024-06-08 10:43:56 +0300

    1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,12 +6,12 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String hostName = "jdbc:mysql://localhost:3306";
-    private static final String userName = "root";
-    private static final String password = "root";
+    private static final String HOST_NAME = "jdbc:mysql://localhost:3306";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
-        return getConnection(hostName, userName, password);
+        return getConnection(HOST_NAME, USER_NAME, PASSWORD);
     }
 
     public static Connection getConnection(String hostName, String userName, String password) throws SQLException {
